1dd70cdb9a5a45226a6cd407d8eb3a28d8e17499
siddharth <110058016+sidish15@users.noreply.github.com>
2024-03-07 16:59:34 +0530

    Delete All Users using ExtraReducers in Redux Toolkit

--- src/store/slices/UserSlices.jsx
+++ src/store/slices/UserSlices.jsx
@@ -18,9 +18,20 @@ const userSlice=createSlice({
                         return [];
                 }, 
         },
+       extraReducers(builder){
+        builder.addCase(userSlice.actions.clearAllUsers,()=>{
+                return [];
+        })
+        // this extraReducer will not work if there is no microreducer(clearAllusers) in the userSlice
+       } 
 });
 
 console.log(userSlice.actions);
 export default userSlice.reducer;
 export const {addUser,removeUser,clearAllUsers} =userSlice.actions;
 // userSlice.reducer :method
+
+/* 
+1. If action is supposed to be handled by one reducer ,use reducers.
+2.If action is supposes to be handled by multiple reducers,use extraReducers.
+*/
